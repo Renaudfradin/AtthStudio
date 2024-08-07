e200818eb9770dee085f3128fdfc6bbbde9b75ac
Renaud <renaudfradin@gmail.com>
2024-08-07 13:00:39 +0200

    (add) allproject page

---
+++ app/allproject/data.js
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    id: 1,
+    title: 'Kūmiio',
+    slug: 'kumiio',
+    release: true,
+  },
+  {
+    id: 2,
+    title: 'SALAMENDERS',
+    slug: 'salamenders',
+    release: true,
+  },
+  {
+    id: 3,
+    title: 'XEANHNORT',
+    slug: 'xeanhnort',
+    release: true,
+  },
+  {
+    id: 4,
+    title: 'FOREST HILL',
+    slug: 'foresthill',
+    release: false,
+  },
+  {
+    id: 5,
+    title: 'MARCO VASCO',
+    slug: 'marcovasco',
+    release: true,
+  },
+  {
+    id: 6,
+    title: 'SUPERMODD',
+    slug: 'supermood',
+    release: true,
+  },
+];
--- app/allproject/page.tsx
+++ app/allproject/page.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import './allproject.css';
 import Top from '../components/top/top';
+import { items } from './data.js';
+import './allproject.css';
+import Image from 'next/image';
+import localFont from 'next/font/local';
+import slider from '@/app/assets/img/allproject/slider.svg';
+import Link from 'next/link';
+
+const monumentExtended = localFont({
+  src: './MonumentExtended-Regular.otf',
+});
 
 export default function page() {
   return (
     <div>
       <Top />
-      <p>all projecty page</p>
+      <div className="content_allproject">
+        <div className="top_allproject">
+          <div>
+            <h2 className={`${monumentExtended.className} title1`}>
+              ALL PROJECT PICK ONE OF THEM
+            </h2>
+            <h2 className={`${monumentExtended.className} title2`}>
+              Don’t be shy
+            </h2>
+          </div>
+          <Image src={slider} className="" alt="Image all project" priority/>
+        </div>
+        <div className='content'>
+          {items.map(({ title, slug, release }) => (
+            <div>
+              <Link className={`${monumentExtended.className} title_list`} href={`project/${slug}`}>{title}</Link>
+              <p>{release}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
