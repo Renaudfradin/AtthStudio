0c704278f0b9be88b509c6ae758144080b031dcc
Renaud <renaudfradin@gmail.com>
2024-08-12 20:23:51 +0200

    (add) 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next';
+import Top from './components/top/top';
+import './globals.css';
+import localFont from 'next/font/local';
+
+export const metadata: Metadata = {
+  title: 'ATTH Studio',
+  description: 'Portfolio Annie Tran ATTH Studio',
+};
+
+const monumentExtended = localFont({
+  src: './MonumentExtended-Regular.otf',
+});
+
+export default function NotFound() {
+  return (
+    <>
+      <Top />
+      <div className="notFound">
+        <h2 className={`${monumentExtended.className}`}>404</h2>
+      </div>
+    </>
+  );
+}
